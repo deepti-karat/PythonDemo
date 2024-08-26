b3b5584b715c8381505cdec0ec0e5ecea447b041
deepti-karat <deeptikarat95@gmail.com>
2024-08-26 17:40:37 +0530

    adding calculator

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+def Calulate():
+    operation = input("Enter an operation (+,-,* or /): ")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    #Add operation:
+    if(operation == "+"):
+        result = num1+num2
+        print(f"Result is {round(result,3)}")
+    # Subtraction operation:
+    elif (operation == "-"):
+        result = num1 - num2
+        print(f"Result is {round(result,3)}")
+    # Multiplication operation:
+    elif (operation == "*"):
+        result = num1 * num2
+        print(f"Result is {round(result,3)}")
+    # Division operation:
+    elif (operation == "/"):
+        result = num1 / num2
+        print(f"Result is {round(result,3)}")
+    # Invalid operation:
+    else:
+        print(f"{operation} is not a valid operation")
+
+Calulate()
