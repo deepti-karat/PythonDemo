75ae668852dd77e3aaac4db40a0b1818c32c1280
deepti-karat <deeptikarat95@gmail.com>
2024-09-01 12:15:27 +0530

    calculator chnage

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,5 @@
 def Calulate():
-    operation = input("Enter an operation (+,-,* or /): ")
+    operation = input("Enter an operation (+,-,*, /, // or %): ")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     #Add operation:
@@ -17,6 +17,14 @@ def Calulate():
     # Division operation:
     elif (operation == "/"):
         result = num1 / num2
+        print(f"Result is {round(result,3)}")
+    # Integer Division
+    elif (operation == "//"):
+        result = num1 // num2
+        print(f"Result is {round(result,3)}")
+    # Remainder (Modulus)
+    elif (operation == "%"):
+        result = num1 % num2
         print(f"Result is {round(result,3)}")
     # Invalid operation:
     else:
